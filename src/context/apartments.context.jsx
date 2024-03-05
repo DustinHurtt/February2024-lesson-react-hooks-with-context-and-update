f01ae0da2234ec6b0c6680a4f238c550b292ca86
ykztenoks <danielskonetzky7@gmail.com>
2024-03-05 19:01:36 -0300

    lab done

--- src/context/apartments.context.jsx
+++ src/context/apartments.context.jsx
@@ -1,35 +1,32 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
-const ApartmentContext = createContext()
+const ApartmentContext = createContext();
 
 const ApartmentProvider = ({ children }) => {
+  const [fetching, setFetching] = useState(true);
+  const [apartments, setApartments] = useState([]);
 
-    const [fetching, setFetching] = useState(true);
-    const [apartments, setApartments] = useState([]);
-  
-  
-    useEffect(() => {
-      console.log("useEffect - Initial render (Mounting)");
-      axios.get("https://ironbnb-m3.herokuapp.com/apartments")
-          .then((response) => {
-              console.log("Apartments data ===>", response.data)
-              setApartments(response.data);
-              setFetching(false);
-          })
-          .catch((err) => {
-              console.log(err)
-              setFetching(false);
-          })
-    }, []);
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    axios
+      .get("https://ironbnb-m3.herokuapp.com/apartments")
+      .then((response) => {
+        console.log("Apartments data ===>", response.data);
+        setApartments(response.data);
+        setFetching(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setFetching(false);
+      });
+  }, []);
 
+  return (
+    <ApartmentContext.Provider value={{ apartments, fetching, setApartments }}>
+      {children}
+    </ApartmentContext.Provider>
+  );
+};
 
-    return (
-        <ApartmentContext.Provider value={{ apartments, fetching}}>
-            {children}
-        </ApartmentContext.Provider>
-    )
-}
-
-
-export { ApartmentContext, ApartmentProvider}
+export { ApartmentContext, ApartmentProvider };
--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -1,13 +1,24 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { ApartmentContext } from "../context/apartments.context";
+import { useNavigate } from "react-router-dom";
 
 const EditListing = () => {
   const [apartment, setApartment] = useState(null);
-
-  const { apartments } = useContext(ApartmentContext);
+  const navigate = useNavigate();
+  const { apartments, setApartments } = useContext(ApartmentContext);
 
   const { id } = useParams();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setApartments((prev) =>
+      prev.map((element) =>
+        element._id === apartment._id ? apartment : element
+      )
+    );
+    navigate("/");
+  };
 
   useEffect(() => {
     let thisApartment = apartments.find((apt) => apt._id === id);
@@ -19,21 +30,54 @@ const EditListing = () => {
   return (
     <>
       {apartment && (
-        <form className="card">
+        <form className="card" onSubmit={handleSubmit}>
           <label>
             Title
-            <input type="text" value={apartment.title} alt="title" />
+            <input
+              type="text"
+              value={apartment.title}
+              name="title"
+              alt="title"
+              onChange={(e) =>
+                setApartment((prev) => ({
+                  ...prev,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+            />
           </label>
           <label>
             Image
-            <input type="text" value={apartment.img} alt="apartment" />
+            <input
+              type="text"
+              value={apartment.img}
+              alt="apartment"
+              name="img"
+              onChange={(e) =>
+                setApartment((prev) => ({
+                  ...prev,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+            />
           </label>
           <label>
             Price
-            <input type="number" value={apartment.pricePerDay} alt="apartment" />
+            <input
+              type="number"
+              value={apartment.pricePerDay}
+              alt="price"
+              name="pricePerDay"
+              onChange={(e) =>
+                setApartment((prev) => ({
+                  ...prev,
+                  [e.target.name]: e.target.value,
+                }))
+              }
+            />
           </label>
 
-          <button>Edit Apartment</button>
+          <button type="submit">Edit Apartment</button>
         </form>
       )}
     </>
